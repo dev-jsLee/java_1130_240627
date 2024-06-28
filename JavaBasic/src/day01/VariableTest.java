75e0d93ef2409a430083ef466a106316c25368fe
dongyeol.shin22 <dongyeol.shin922@gmail.com>
2024-06-28 15:11:30 +0900

    day02

---
+++ JavaBasic/src/day01/VariableTest.java
@@ -0,0 +1,26 @@
+package day01;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world.");
+		// 출력문을 작성하는 이유
+		// 코드를 쓰는 중간에 오류의 위치나 값을 확인하기 위해
+		
+		// int, long
+		int num;  // 선언
+		num = 10; // 값을 할당
+		
+		// 변수의 선언과 할당을 동시에 한다.
+		long numLong = 1000L;
+		long numLong2 = 1000;
+		
+		
+		// float, double
+		// char, String
+		
+		
+
+	}
+
+}
